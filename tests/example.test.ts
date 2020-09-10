c80c828ac45373f377e66fd42e6742b3bd3100e6
John Leonardo <j@jdleo.me>
2020-09-10 10:15:36 -0500

    added example test suite

---
+++ tests/example.test.ts
@@ -0,0 +1,19 @@
+describe('First test component', () => {
+  it('should be true when true', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should be false when false', () => {
+    expect(false).toBe(false);
+  });
+});
+
+describe('Second test component', () => {
+  it('should be 4 when 2+2', () => {
+    expect(2 + 2).toBe(4);
+  });
+
+  it('should be 30 when 5*6', () => {
+    expect(5 * 6).toBe(30);
+  });
+});
